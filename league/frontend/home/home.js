ac386662a4f14ec3a92e520e5ce35470e051b6da
Berty Ruan <berty.ruan@gmail.com>
2015-10-27 21:16:56 -0400

    adds some html

--- league/frontend/home/home.js
+++ league/frontend/home/home.js
@@ -1,6 +1,6 @@
 var HomeModule = angular.module('HomeModule', []);
 
 HomeModule.controller("HomeController", ['$scope', '$stateParams', 'Item', function($scope, $stateParams, Item){
-	$scope.item = Item.query();
+	$scope.items = Item.query();
 
 }]);
